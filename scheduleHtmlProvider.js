b4b02b69c25b929684e66fae6ecc9b124b72516e
MoeShin <hi@moeshin.com>
2021-03-09 15:39:05 +0800

    直接打开课表

--- scheduleHtmlProvider.js
+++ scheduleHtmlProvider.js
@@ -1,5 +1,15 @@
 function scheduleHtmlProvider(iframeContent = "", frameContent = "", dom = document) {
-    return dom.querySelector('iframe[data-id*=queryKbForXsd]')
-            .contentDocument.querySelector('table')
-            .outerHTML;
+    const iframe = dom.querySelector('iframe[src$="/queryKbForXsd"]');
+    const path = '/admin/pkgl/xskb/queryKbForXsd';
+    let doc;
+    if (location.pathname.replace(/\/+/g, '/').replace(/\/$/, '') === path) {
+        doc = dom;
+    } else if (iframe) {
+        doc = iframe.contentDocument;
+    } else {
+//         alert('「首页」的「个人课表」并不完整！请点击「个人课表」日期右边的省略按钮，或者「学生端」>「信息查询」>「我的课表」');
+        open(path, '_self');
+        return;
+    }
+    return doc.querySelector('table').outerHTML;
 }
